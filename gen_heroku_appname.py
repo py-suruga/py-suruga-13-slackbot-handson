a5419e3eaa1df5845ed3a3479fcc814a4666a226
hirohis sano <hrs.sano645@gmail.com>
2020-05-21 11:27:12 +0900

    herokuのアプリ名を決めるスクリプト

---
+++ gen_heroku_appname.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+
+# herokuのアプリ名を決めるスクリプト
+# DEFAULT_PREFIXのあとに数字6桁をランダムで生成する
+
+import random
+
+DEFAULT_PREFIX = "pysuruga13-slackbot"
+
+
+def main():
+    random_number = random.randint(1, 999999)
+    print("生成したアプリ名は '{}-{:06}'".format(DEFAULT_PREFIX, random_number))
+
+
+if __name__ == "__main__":
+    main()
