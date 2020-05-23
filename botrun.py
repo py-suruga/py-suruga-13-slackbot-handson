bb5e38a133e2b9e62e145e9a4bd8c3418c5096c8
hirohis sano <hrs.sano645@gmail.com>
2020-05-23 15:20:29 +0900

    [wip] use blockkit

--- botrun.py
+++ botrun.py
@@ -2,12 +2,31 @@
 import os
 import re
 import sys
+import json
 
 from flask import Flask
 from slack import WebClient
 from slackeventsapi import SlackEventAdapter
 
 import requests
+
+
+# Livedoor Weather REST API
+# code一覧
+# <city title="静岡" id="220010" source="http://weather.livedoor.com/forecast/rss/area/220010.xml"/>
+# <city title="網代" id="220020" source="http://weather.livedoor.com/forecast/rss/area/220020.xml"/>
+# <city title="三島" id="220030" source="http://weather.livedoor.com/forecast/rss/area/220030.xml"/>
+# <city title="浜松" id="220040" source="http://weather.livedoor.com/forecast/rss/area/220040.xml"/>
+LD_TENKI_API_ENDPOINT = "http://weather.livedoor.com/forecast/webservice/json/v1"
+
+# 取得する天気のコードとコードの地域名のマップ
+LD_TENIKI_CITY_CODE_MAPS = {
+    "中部": "220010",
+    "伊豆": "220020",
+    "東部": "220030",
+    "西部": "220040",
+}
+
 
 # Flaskを作ってgunicornで動くようにする
 app = Flask(__name__)
@@ -66,26 +85,11 @@ def tenki(event_data):
     # Livedoor 天気予報をきく
     # shizuokatenki [西部,中部,東部,伊豆,]
     # 今日の天気は ** 気温は**℃です！
-
-    code一覧
-    <city title="静岡" id="220010" source="http://weather.livedoor.com/forecast/rss/area/220010.xml"/>
-    <city title="網代" id="220020" source="http://weather.livedoor.com/forecast/rss/area/220020.xml"/>
-    <city title="三島" id="220030" source="http://weather.livedoor.com/forecast/rss/area/220030.xml"/>
-    <city title="浜松" id="220040" source="http://weather.livedoor.com/forecast/rss/area/220040.xml"/>
     """
-    api_url = "http://weather.livedoor.com/forecast/webservice/json/v1"
 
     # botが反応する正規表現パターン
     message_pattern = "shizuokatenki\\s(.{2})"
 
-    # 取得する天気のコードとコードの地域名のマップ
-    city_code_map = {
-        "中部": "220010",
-        "伊豆": "220020",
-        "東部": "220030",
-        "西部": "220040",
-    }
-    
     print("debug:handled function: {}".format(sys._getframe().f_code.co_name))
     print("debug:eventdata:{}".format(event_data))
     message = event_data["event"]
@@ -97,18 +101,18 @@ def tenki(event_data):
         if matchobj:
             # 地域名が存在するか確認する
             city_name = matchobj.group(1)
-            if city_name in city_code_map:
-                print("run tenki ")
+            if city_name in LD_TENIKI_CITY_CODE_MAPS:
+                print("debug:run tenki ")
 
                 # API経由で天気を調べる
-                city_code = city_code_map[matchobj.group(1)]
+                city_code = LD_TENIKI_CITY_CODE_MAPS[matchobj.group(1)]
 
                 payload = {"city": city_code}
-                api_response = requests.get(api_url, params=payload)
+                api_response = requests.get(LD_TENKI_API_ENDPOINT, params=payload)
                 # http://weather.livedoor.com/forecast/webservice/json/v1?code=220010
 
                 result = api_response.json()
-                print("result_obj:{}".format(result))
+                print("debug:result_obj:{}".format(result))
                 # コマンド実行時の今日の天気予報を抽出
                 weather_telop = result["forecasts"][0]["telop"]
                 weather_temp = result["forecasts"][0]["temperature"]["max"]
@@ -123,7 +127,135 @@ def tenki(event_data):
                 slack_client.chat_postMessage(channel=channel, text=res_message)
 
 
-# TODO:2020-05-23:時間があればIntaractive Messageの実装を行う
+def generate_shizutenki_button(city_code_maps: list) -> str:
+    """
+    Blockkitを使ってボタンあり構造を生成する
+    """
+    question_block_base_json = """
+    	{
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "コンニチハ！:robot_face: 私はpysurugabotです！:mount_fuji::shrimp::fish:\n静岡の今日の天気をお答えします。地域を選択してください。"
+            }
+        },
+        {
+            "type": "actions",
+            "elements": [
+            ]
+        }
+    """
+    question_block_button_base_json = """
+        {
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "CITY_NAME"
+            },
+            "style": "primary",
+            "value": "shizutenki_123"
+        }
+    """
+
+    block = json.loads(question_block_base_json)
+
+    block_button_list = list()
+    for city_code in city_code_maps.keys():
+
+        block_button = json.loads(question_block_button_base_json)
+        block_button["text"]["text"] = city_code
+        block_button_list.append(block_button)
+    
+    block["elements"] = block_button_list
+
+    return json.dumps(block)
+
+
+@slack_events_adapter.on("message")
+def tenki_button_question(event_data):
+    """
+    # Livedoor 天気予報を聞く Intaractiveバージョン
+    # shizutenki [西部,中部,東部,伊豆,]
+    # 今日の天気は ** 気温は**℃です！
+    """
+
+    # botが反応する正規表現パターン
+    message_pattern = "shizutenki"
+    
+    print("debug:handled function: {}".format(sys._getframe().f_code.co_name))
+    print("debug:eventdata:{}".format(event_data))
+    message = event_data["event"]
+
+    # subtypeがない場合=普通のメッセージ, 自分自身の内容を取得してもスルーするようにしておく必要がある
+    if message.get("subtype") is None and message.get("bot_id") is None:
+        # botのパターンとして認識する文字がある場合
+        matchobj = re.match(message_pattern, message.get("text"))
+        if matchobj:
+            # メッセージを返す
+            channel = message["channel"]
+            res_message = generate_shizutenki_button(LD_TENIKI_CITY_CODE_MAPS)
+            slack_client.chat_postMessage(channel=channel, text=res_message)
+
+
+@slack_events_adapter.on("message")
+def tenki_button_ansewer(event_data):
+    
+        print("debug:handled function: {}".format(sys._getframe().f_code.co_name))
+        print("debug:eventdata:{}".format(event_data))
+        message = event_data["event"]
+
+# @slack_events_adapter.on("message")
+# def tenki_button_ansewer(event_data):
+#     """
+#     tenki_button_
+#     """
+#         """
+#     # Livedoor 天気予報を聞く Intaractiveバージョン
+#     # shizutenki [西部,中部,東部,伊豆,]
+#     # 今日の天気は ** 気温は**℃です！
+#     """
+
+#     # botが反応する正規表現パターン
+#     message_pattern = "shizutenki"
+    
+#     print("debug:handled function: {}".format(sys._getframe().f_code.co_name))
+#     print("debug:eventdata:{}".format(event_data))
+#     message = event_data["event"]
+
+#     # subtypeがない場合=普通のメッセージ, 自分自身の内容を取得してもスルーするようにしておく必要がある
+#     if message.get("subtype") is None and message.get("bot_id") is None:
+        
+        
+#         # botのパターンとして認識する文字がある場合
+#         matchobj = re.match(message_pattern, message.get("text"))
+#         if matchobj:
+#             # 地域名が存在するか確認する
+#             city_name = matchobj.group(1)
+#             if city_name in LD_TENIKI_CITY_CODE_MAPS:
+#                 print("debug:run tenki button ")
+
+#                 # API経由で天気を調べる
+#                 city_code = LD_TENIKI_CITY_CODE_MAPS[matchobj.group(1)]
+
+#                 payload = {"city": city_code}
+#                 api_response = requests.get(LD_TENKI_API_ENDPOINT, params=payload)
+#                 # http://weather.livedoor.com/forecast/webservice/json/v1?code=220010
+
+#                 result = api_response.json()
+#                 print("debug:result_obj:{}".format(result))
+#                 # コマンド実行時の今日の天気予報を抽出
+#                 weather_telop = result["forecasts"][0]["telop"]
+#                 weather_temp = result["forecasts"][0]["temperature"]["max"]
+                
+#                 if weather_temp is None:
+#                     res_message = "静岡県{}の今日の天気は {} です！".format(city_name, weather_telop)
+#                 else:
+#                     res_message = "静岡県{}の今日の天気は {} 気温は{}℃です！".format(city_name, weather_telop, weather_temp["celsius"])
+#                 # メッセージを返す
+#                 channel = message["channel"]
+                
+#                 slack_client.chat_postMessage(channel=channel, text=res_message)
 
 # エラー時のイベントのハンドリング
 @slack_events_adapter.on("error")
